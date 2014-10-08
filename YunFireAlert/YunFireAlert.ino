1a9d3240189cb396fe3a83dd1e4b87c22ea7c903
arve0 <arve.seljebu@gmail.com>
2014-10-08 20:03:36 +0200

    clean up structure, add twilio check

--- YunFireAlert/YunFireAlert.ino
+++ YunFireAlert/YunFireAlert.ino
@@ -10,12 +10,103 @@ Check inputs and alert upon fire alarm or failure.
 #include <Process.h>
 #include "MsTimer2.h"
 
+
+/*
+ * Variables
+ */
 // pins
 const byte firePin = 2;
 const byte failInPin = 3;
 const byte failOutPin = 4;
 const byte statusPin = 13;
+// status codes
+const byte FIRE = 0;
+const byte FAILURE = 1;
+const byte OK = 10;
 
+
+/*
+ * Functions
+ */
+void toggleStatus() {
+  // toggle statusPin
+  static byte out = HIGH;
+  digitalWrite(statusPin, out);
+  out = !out;
+}
+
+void blink(unsigned int period){
+  /* blink with MsTimer2
+   * 
+   * :param period: duty cycle of blinking in milliseconds
+   */
+  if (period == 0){
+    MsTimer2::stop();
+    digitalWrite(statusPin, LOW);
+  }
+  else {
+    MsTimer2::stop();
+    MsTimer2::set(period, toggleStatus);
+    MsTimer2::start();
+  }
+}
+
+byte getState(){
+  /* Get current state and return it.
+   *
+   * :globals: firePin, failInPin
+   * :return: constant FIRE, FAILURE or OK
+   */
+  byte fire = digitalRead(firePin);
+  byte failure = digitalRead(failInPin);
+  delay(50); // debounce
+  if (fire == 1 && digitalRead(firePin) == 1)
+    return FIRE;
+  else if (failure == 0 && digitalRead(failInPin) == 0)
+    return FAILURE;
+  else
+    return OK;
+}
+
+void smsAlert(char type[]){
+  /* send SMS through twilio with sms_alert.py
+   * 
+   * :param type: "fire", "failure", "ok" or "test"
+   */
+  Process p;
+  p.begin("/root/sms_alert.py");
+  p.addParameter(type);
+  p.run();
+  while (p.available()>0){
+    char c = p.read();
+    Console.write(c);
+  }
+  Console.flush();    
+}
+
+byte checkTwilio(){
+  /* check connection to twilio with python script
+   *
+   * :return: exit code python script
+   */
+  Process p;
+  p.begin("/root/check_twilio.py");
+  p.run();
+  if (p.available()>0) {
+    Console.println("No Twilio connection. Error:");
+  }
+  while (p.available()>0){
+    char c = p.read();
+    Console.write(c);
+  }
+  Console.flush();
+  return p.exitValue();
+}
+
+
+/*
+ * Runtime
+ */
 void setup() {
   // relay normally open with external pullup
   pinMode(failOutPin, OUTPUT);
@@ -31,63 +122,16 @@ void setup() {
 
   Bridge.begin();
   Console.begin();
-}
-
-void toggleStatus() {
-  // toggle statusPin
-  static byte out = HIGH;
-  digitalWrite(statusPin, out);
-  out = !out;
-}
-
-void blink(unsigned int period){
-  if (period == 0){
-    MsTimer2::stop();
-    digitalWrite(statusPin, LOW);
+  while (checkTwilio() != 0) {
+    Console.println("Waiting for internet connection to Twilio.");
+    delay(1000);
   }
-  else {
-    MsTimer2::stop();
-    MsTimer2::set(period, toggleStatus);
-    MsTimer2::start();
-  }
-}
-
-// status codes
-const byte FIRE = 0;
-const byte FAILURE = 1;
-const byte OK = 10;
-
-byte getState(){
-  // Get current state and return it.
-  byte fire = digitalRead(firePin);
-  byte failure = digitalRead(failInPin);
-  delay(50); // debounce
-  if (fire == 1 && digitalRead(firePin) == 1)
-    return FIRE;
-  else if (failure == 0 && digitalRead(failInPin) == 0)
-    return FAILURE;
-  else
-    return OK;
-}
-
-void smsAlert(char type[]){
-  // send SMS through twilio with sms_alert.py
-  // type: "fire", "failure", "ok" or "test"
-  Process p;
-  p.begin("/root/sms_alert.py");
-  p.addParameter(type);
-  p.run();
-  while (p.available()>0){
-    char c = p.read();
-    Console.write(c);
-  }
-  Console.flush();    
 }
 
 void loop() {
-  // initial states
+  // initial state (previous FAILURE -> send OK alert upon start)
   static byte currentState = OK;
-  static byte previousState = OK;
+  static byte previousState = FAILURE;
 
   // get current state
   currentState = getState();
