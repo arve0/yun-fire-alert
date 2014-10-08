74d6b20561ecffed11cee223fb1bfb8051561ade
arve0 <arve.seljebu@gmail.com>
2014-10-08 10:17:39 +0200

    alert failures also

--- sms_alert.py
+++ sms_alert.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 '''
-Send an SMS alert through Twilio.
+SMS alert through Twilio.
+
+Usage: ./sms_alert.py (fire|failure|test)
 '''
 
-# put your own credentials here
+# put your twilio credentials here
 ACCOUNT_SID = ''
 AUTH_TOKEN = ''
 FROM_NUMBER = ''
@@ -13,20 +15,28 @@ FROM_NUMBER = ''
 FROM_EMAIL = ''
 SMTP_SERVER = ''
 
-# put the reciever numbers here, including +country
+# receivers of alarms and failures
 NUMBERS = [
         '+47XXXXXXXX',
         '+47XXXXXXXX'
         ]
+# receivers of alarms only
+ALARM_NUMBERS = [
+        '+47XXXXXXXX'
+        ]
+ALARM_NUMBERS.extend(NUMBERS)
 
-# email adresses in case of twilio failing
+# email adresses in case of sms failing
 EMAIL_ADRESSES = [
         'you@domain.com',
         'you2@domain.com'
         ]
 
 # alert message
-MESSAGE = 'Fire alarm alert!'
+ALARM_MESSAGE = 'Fire alarm alert!'
+
+# alert message
+FAILURE_MESSAGE = 'Fire alarm failure.'
 
 # No edit beyond this point should be necessary unless you want to change the
 # behaviour of this script!
@@ -36,31 +46,46 @@ from twilio.rest.exceptions import TwilioRestException
 from twilio.rest import TwilioRestClient
 import smtplib
 from email.mime.text import MIMEText
+import sys
 
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
-# send SMSes
-for number in NUMBERS:
-    try:
-        client.messages.create(
-            from_=FROM_NUMBER,
-            to=number,
-            body=MESSAGE)
-    except TwilioRestException as err:
-        # error handling, send error message to adresses in EMAIL_ADRESSES
-        body = ('Message: ' + MESSAGE + '\n'
-                'Twilio error message: ' + err.msg + '\n'
-                'Method: ' + err.method + '\n'
-                'Status: ' + str(err.status) + '\n'
-                'Uri: ' + err.uri)
-        msg = MIMEText(body)
-        msg['Subject'] = 'Twilio Error'
-        msg['From'] = FROM_EMAIL
-        s = smtplib.SMTP(SMTP_SERVER)
-        msg['To'] = ', '.join(EMAIL_ADRESSES)
-        s.sendmail(FROM_EMAIL, EMAIL_ADRESSES, msg.as_string())
-        s.quit()
-        print('Twilio error: ' + err.msg)
-        exit(1)
+# send SMSe
+def smsAlert(message, numbers):
+    for number in numbers:
+        try:
+            client.messages.create(
+                from_=FROM_NUMBER,
+                to=number,
+                body=message)
+        except TwilioRestException as err:
+            # error handling, send error message to adresses in EMAIL_ADRESSES
+            body = ('Message: ' + message + '\n'
+                    'Twilio error message: ' + err.msg + '\n'
+                    'Method: ' + err.method + '\n'
+                    'Status: ' + str(err.status) + '\n'
+                    'Uri: ' + err.uri)
+            msg = MIMEText(body)
+            msg['Subject'] = 'Twilio Error'
+            msg['From'] = FROM_EMAIL
+            s = smtplib.SMTP(SMTP_SERVER)
+            msg['To'] = ', '.join(EMAIL_ADRESSES)
+            s.sendmail(FROM_EMAIL, EMAIL_ADRESSES, msg.as_string())
+            s.quit()
+            print('Twilio error: ' + err.msg)
+            exit(1)
+        else:
+            print('Alert sent to ' + ', '.join(NUMBERS))
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'fire':
+            smsAlert(FIRE_MESSAGE, ALARM_NUMBERS)
+        elif sys.argv[1] == 'failure':
+            smsAlert(FAILURE_MESSAGE, NUMBERS)
+        elif sys.argv[1] == 'test':
+            smsAlert('This is an test from sent from yun-fire-alert.', NUMBERS)
+        else:
+            print('''Usage: [fire|failure|test]'''.format(sys.argv[0]))
     else:
-        print('Alert sent to ' + ', '.join(NUMBERS))
+        print('''Usage: [fire|failure|test]'''.format(sys.argv[0]))
