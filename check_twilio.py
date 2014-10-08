5ce91d26116273addc0b699b9f43d15ef1773355
arve0 <arve.seljebu@gmail.com>
2014-10-08 18:11:54 +0200

    script for checking access to twilio api

---
+++ check_twilio.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+yun-fire-alert
+Author: Arve Seljebu
+Code: https://github.com/arve0/yun-fire-alert
+
+Check if we got connection to Twilio API.
+"""
+import httplib2, os
+
+# use bundled certificate file
+http = httplib2.Http(ca_certs=os.path.abspath('cacert.pem'))
+http.follow_redirects = False
+
+try:
+    response, content = http.request('https://api.twilio.com/2010-04-01')
+except Exception as error:
+    print(error)
+    exit(1)
+else:
+    if response.status == 200 and response['content-type'] == 'application/xml':
+        exit(0)
+    elif response.status != 200:
+        print('Got response status {status}'.format(**response))
+        exit(1)
+    else:
+        print('Response not xml. Content-type: {content-type}'.format(**response))
+        exit(1)
